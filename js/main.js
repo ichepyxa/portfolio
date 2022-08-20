f459218ec550672e258f1473dda32dc5d601e717
Максим Сарамудов <maxim.krytizna@gmail.com>
2022-08-21 02:47:42 +0300

    feat: add cv, facts, tabs, portfolio, skills, footer

--- js/main.js
+++ js/main.js
@@ -28,9 +28,50 @@ const typedText = element => {
 	})
 }
 
+const tabsToggle = (tabsBtns, tabsContents) => {
+	const removeActiveClass = element => {
+		element.classList.remove('active')
+	}
+
+	const addActiveClass = element => {
+		element.classList.add('active')
+	}
+
+	const clearActiveTabs = (btns, contents) => {
+		btns.forEach(btn => removeActiveClass(btn))
+		contents.forEach(content => removeActiveClass(content))
+	}
+
+	tabsBtns.forEach((btn, index) => {
+		btn.addEventListener('click', e => {
+			if (!e.target.classList.contains('active')) {
+				clearActiveTabs(tabsBtns, tabsContents)
+				addActiveClass(tabsContents[index])
+				addActiveClass(e.target)
+			}
+		})
+	})
+}
+
+const initSkills = skills => {
+	skills.forEach(skill => {
+		console.log(skill.querySelector('.skills-item__percent'))
+		const progress = skill.querySelector('.skills-item__progress')
+		const percent = `${progress.dataset.percent}%`
+
+		skill.querySelector('.skills-item__percent').innerText = percent
+		progress.querySelector('span').style.width = percent
+	})
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	typedText(document.getElementById('typed-text'))
 	checkTheme(document.getElementById('page'))
+	initSkills(document.querySelectorAll('.skills-items .skills-item'))
+	tabsToggle(
+		document.querySelectorAll('#tabs button'),
+		document.querySelectorAll('#tabs .tabs-content')
+	)
 })
 
 document.getElementById('btn-theme').addEventListener('click', () => {
